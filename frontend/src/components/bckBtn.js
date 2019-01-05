5e9f037b1a28edbb28f88609e5d31c0960b6d6aa
SheinaT <Sheina.tolmas@gmail.com>
2019-01-05 16:42:35 +0200

    back button rendered

--- frontend/src/components/bckBtn.js
+++ frontend/src/components/bckBtn.js
@@ -1,17 +1,18 @@
 import React from "react";
 
 export default class BckBtn extends React.Component {
-    constructor(props){
-        super(props);
+
+    goBack() {
+        console.log("in")
+        window.history.go(-1)
+
     }
 
-    render(){
-        return(
-        <div className="back-btn">
-        <a href="../games">Back to games page</a>
-        </div>
-    )
 
+    render() {
+        return(
+        <button className='back-btn' onClick={this.goBack.bind(this)}>Back</button>
+        )
     }
 
 }
